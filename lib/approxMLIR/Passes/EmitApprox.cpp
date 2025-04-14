9179da39d2fdc17aa87dec80b44736b301a65580
Hao Ren <moomoohorse738834256@gmail.com>
2025-04-13 21:24:32 -0500

    to debug

--- lib/approxMLIR/Passes/EmitApprox.cpp
+++ lib/approxMLIR/Passes/EmitApprox.cpp
@@ -98,15 +98,21 @@ namespace {
         }
     } 
 
-    struct FuncOpConversion : public OpConversionPattern<func::FuncOp> 
+    static void dump_region(Region* region) {
+        for (Block &block : region->getBlocks()) {
+            block.dump();
+        }
+    }
+
+    struct AnnocationOpConversion : public OpConversionPattern<func::CallOp> 
     {
-        using OpConversionPattern<func::FuncOp>::OpConversionPattern;
+        using OpConversionPattern<func::CallOp>::OpConversionPattern;
         
         /**
          * For each function, we scan through it, finding annotations that mark the region to insert in our knobOp (in this case, approxForOp).
          */
         LogicalResult
-        matchAndRewrite(func::FuncOp funcOp, OpAdaptor adaptor, 
+        matchAndRewrite(func::CallOp callOp, OpAdaptor adaptor, 
             ConversionPatternRewriter &rewriter) const final {
             std::vector<Operation*> opsToMove;
             std::vector<std::pair<Type, Location>> producers;
@@ -114,14 +120,11 @@ namespace {
             std::vector<Operation*> users;
             std::vector<Value> results;
             std::vector<Type> resultTypes;
-
-            Region & region = funcOp.getBody();
+            
             Operation* start_knob = nullptr;
             Operation* end_knob = nullptr;
 
-            auto savedInsertPoint = rewriter.saveInsertionPoint();
-            int in_knob = -1;
-            
+
             // decision tree arguments
             Value state;
             Value num_thresholds;
@@ -130,21 +133,45 @@ namespace {
             Value decision_values;
             Value thresholds;
             Value decisions;
+
+            bool in_knob = false;
+            Region* region = nullptr;
             
-            funcOp.dump();
+            callOp.dump();
+            
+            StringRef callee = callOp.getCallee();
+            
+            llvm::outs() << callee << "\n";
+
+            // We only look at the start and get the parent region.
+            if(callee.compare(StringRef("knob_start")) != 0) {
+                llvm::outs() << "not knob_start\n";
+                return success();
+            }
+            
+            // callOp.dump();
             
             
-            // step 1: mark the ops to move
-            for (Block &block : region.getBlocks()) {
+            region = callOp->getParentRegion();
+
+            dump_region(region);
+            
+
+            // step 1: mark the ops to move (we only look at the first knob_start and knob_end)
+            for (Block &block : region->getBlocks()) {
                 for (Operation &op : block.getOperations()) {
                     // check if hit the user annotation "func.call @knob_start() : () -> ()"
-                    if (-1 == in_knob) {
-                        auto callOp = dyn_cast<func::CallOp>(op);
-                        if (callOp.getCallee() == "knob_start") {
+                    if (!in_knob) {
+                        auto _callOp = dyn_cast<func::CallOp>(op);
+                        if (!_callOp) {
+                            continue;
+                        }
+                        if (_callOp.getLoc() == callOp.getLoc()) {
                             in_knob = true;
                             start_knob = &op; // we will later replace this will the approxOp
                         }
-                        if (callOp.getCallee() == "knob_end") {
+
+                        if (_callOp.getCallee().compare(StringRef("knob_end")) == 0) {
                             in_knob = false;
                             end_knob = &op; // we will later replace this will the approxOp
                         }
@@ -152,55 +179,55 @@ namespace {
                     }
                     if(in_knob) {
                         opsToMove.push_back(&op);
-                        // then we keep track of all the uses for the internal Ops
-                        // also we keep track of all the defs for the internal Ops
-                        // such uses and defs will be used to create the KnobOp
-                        track_users(op, users, results);
-                        track_producers(op, producers, producerVals);
                     }
                 }
             }
             
-            if(start_knob == nullptr || end_knob == nullptr) {
-                return success();
-            }
+            llvm::outs() << "here\n";
+
+            auto savedInsertPoint = rewriter.saveInsertionPoint();
             
+            if(start_knob == nullptr || end_knob == nullptr) {
+                return failure();
+            }
+
+            start_knob->dump();
+            end_knob->dump();
+
             // First emit Ops to the new block, then move the new block to the body of the approxForOp
-            Block* tempBlock = rewriter.createBlock(&region, {}, std::nullopt, std::nullopt); // side-effect: change insert point to the end of the created block
+            Block* tempBlock = rewriter.createBlock(region, {}, std::nullopt, std::nullopt); // side-effect: change insert point to the end of the created block
 
             generate_approx_outs(opsToMove, users, results, resultTypes);
 
             // step 2: move Ops to the new block
-            for (Block &block : region.getBlocks()) {
-                for (Operation &op : block.getOperations()) {
-                    if(dyn_cast<func::CallOp>(op).getCallee() == "decision_tree") {
-                        // parse func.call @decision_tree(%i_f32, %num_thresholds, %thresholds_uppers, %thresholds_lowers, %decision_values, %thresholds, %decisions) : (f32, i32, tensor<3xf32>, tensor<3xf32>, tensor<3xi32>, tensor<3xf32>, tensor<3xi32>) -> () 
-                        // then create a decideOp, inserting it into the tempBlock
-                        if (auto callOp = dyn_cast<func::CallOp>(op)) {
-                            auto callee = callOp.getCallee();
-                            if (callee == "decision_tree") {
-                                // parse the inputs
-                                state = callOp.getOperand(0);
-                                num_thresholds = callOp.getOperand(1);
-                                thresholds_uppers = callOp.getOperand(2);
-                                thresholds_lowers = callOp.getOperand(3);
-                                decision_values = callOp.getOperand(4);
-                                thresholds = callOp.getOperand(5);
-                                decisions = callOp.getOperand(6);
-
-                                // create the decideOp
-                                // then, we insert the decideOp based on the annotation func.call @decision_tree(%i_f32, %num_thresholds, %thresholds_uppers, %thresholds_lowers, %decision_values, %thresholds, %decisions) : (f32, i32, tensor<3xf32>, tensor<3xf32>, tensor<3xi32>, tensor<3xf32>, tensor<3xi32>) -> ()
-                                rewriter.create<approxMLIR::decideOp>(op.getLoc(), 
-                                    state, num_thresholds, thresholds_uppers, thresholds_lowers, decision_values, thresholds, decisions);
-                                
-                                // moveMarkedOpsToNewBlock(op, tempBlock, rewriter);
-                                break;
-                            }
-                        }
-                    }
+            for (auto* _op : opsToMove) {
+                Operation & op = *_op;
+                if(dyn_cast<func::CallOp>(op) && dyn_cast<func::CallOp>(op).getCallee().compare(StringRef("decision_tree")) == 0) {
+                    // parse func.call @decision_tree(%i_f32, %num_thresholds, %thresholds_uppers, %thresholds_lowers, %decision_values, %thresholds, %decisions) : (f32, i32, tensor<3xf32>, tensor<3xf32>, tensor<3xi32>, tensor<3xf32>, tensor<3xi32>) -> () 
+                    // then create a decideOp, inserting it into the tempBlock
+                    auto callOp = dyn_cast<func::CallOp>(op);
+                    // parse the inputs
+                    state = callOp.getOperand(0);
+                    num_thresholds = callOp.getOperand(1);
+                    thresholds_uppers = callOp.getOperand(2);
+                    thresholds_lowers = callOp.getOperand(3);
+                    decision_values = callOp.getOperand(4);
+                    thresholds = callOp.getOperand(5);
+                    decisions = callOp.getOperand(6);
+                    
+                    // create the decideOp
+                    // then, we insert the decideOp based on the annotation func.call @decision_tree(%i_f32, %num_thresholds, %thresholds_uppers, %thresholds_lowers, %decision_values, %thresholds, %decisions) : (f32, i32, tensor<3xf32>, tensor<3xf32>, tensor<3xi32>, tensor<3xf32>, tensor<3xi32>) -> ()
+                    rewriter.replaceOpWithNewOp<approxMLIR::decideOp>(_op, 
+                        state, num_thresholds, thresholds_uppers, thresholds_lowers, decision_values, thresholds, decisions);
+                            
                 }
+                // then we keep track of all the uses for the internal Ops
+                // also we keep track of all the defs for the internal Ops
+                // such uses and defs will be used to create the KnobOp
+                track_users(op, users, results);
+                track_producers(op, producers, producerVals);
             }
-
+            
             for (auto* op : opsToMove) {
                 // here we use rewriter to move the Ops between the 2 annotations in the new block
                 moveMarkedOpsToNewBlock(op, rewriter);   
@@ -241,11 +268,12 @@ namespace {
             });
 
             RewritePatternSet patterns(&getContext());
-            patterns.add<FuncOpConversion>(&getContext());
-
-            if(failed(applyPatternsAndFoldGreedily(getOperation(), std::move(patterns))))
+            patterns.add<AnnocationOpConversion>(&getContext());
+            GreedyRewriteConfig config;
+            config.maxIterations = 1;
+            if(failed(applyPatternsAndFoldGreedily(getOperation(), std::move(patterns), config))) {
                 return signalPassFailure();
-
+            }
         }
     };
 }
